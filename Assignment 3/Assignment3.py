62005791accaef437959b2ab4a9933f831329af5
Trey West <rew7y@virginia.edu>
2018-11-09 17:44:44 -0600

    Trying to re-implement data structures as Pandas DataFrames

--- Assignment 3/Assignment3.py
+++ Assignment 3/Assignment3.py
@@ -9,60 +9,85 @@ data = data.dropna()
 
 def KMeans(K, data, dist_measure = "euclidean"):
     # Initialize K centroids randomly
-    centroids = []
+    centroids = pd.DataFrame(columns = data.columns, index = range(K))
     for i in range(K):
-        centroids.append([])
         for feature in data.columns:
-            centroids[i].append(uniform(data[feature].min(), data[feature].max()))
+            centroids.at[i, feature] = uniform(data[feature].min(), data[feature].max())
     
-    # Initialize centroid_assignments data structure
-    centroid_assignments = {}
-    for i in range(K):
-        centroid_assignments[i] = []
-    
-    # Assign all data to centroids
+    # Assign data to clusters
+    cluster_assignments = pd.DataFrame(columns = ["Cluster"], index = data.index)
     for index, row in data.iterrows():
-        centroid_assignments[assign_to_cluster(centroids, row, dist_measure)].append(index)
+        cluster_assignments.at[index, "Cluster"] = assign_to_cluster(centroids, row, dist_measure)
+    
+    # Recompute centroids
+    averages = pd.DataFrame(columns = data.columns, index = range(K))
+    averages.fillna(0)
+    for index, row in cluster_assignments.iterrows():
+        for feature in data.columns:
+            averages.at[row["Cluster"], feature] += data.at[index, feature]
+    
+    for index, row in averages.iterrows():
+        for feature in data.columns:
+            
+    
+    return centroids, centroid_assignments
 
-    while True:
-        # Recompute centroids
-        for centroid in centroid_assignments:
-            # Reset averages of all features to 0
-            averages = []
-            for i in range(len(data.columns)):
-                averages.append(0)
-            
-            # Iterate over samples in this cluster,
-            # adding their features to average
-            for sample in centroid_assignments[centroid]:
-                for i in range(len(data.columns)):
-                    averages[i] += data.at[sample, data.columns[i]]
-            
-            # Divide all sums by number of samples in cluster to get average
-            for i in range(len(averages)):
-                if(len(centroid_assignments[centroid]) != 0):
-                    averages[i] /= len(centroid_assignments[centroid])
-            
-            # Re-assign centroid values
-            for i in range(len(centroids[centroid])):
-                centroids[centroid][i] = averages[i]
-        
-        # Reassign data to clusters
-        new_centroid_assignments = {}
-        for i in range(K):
-            new_centroid_assignments[i] = []
-        
-        for index, row in data.iterrows():
-            new_centroid_assignments[assign_to_cluster(centroids, row, dist_measure)].append(index)
-        
-        # If clusters have not changed, we're done
-        if new_centroid_assignments == centroid_assignments:
-            return centroid_assignments
-        else:
-            centroid_assignments = new_centroid_assignments
+#def KMeans(K, data, dist_measure = "euclidean"):
+#    # Initialize K centroids randomly
+#    centroids = []
+#    for i in range(K):
+#        centroids.append([])
+#        for feature in data.columns:
+#            centroids[i].append(uniform(data[feature].min(), data[feature].max()))
+#    
+#    # Initialize centroid_assignments data structure
+#    centroid_assignments = {}
+#    for i in range(K):
+#        centroid_assignments[i] = []
+#    
+#    # Assign all data to centroids
+#    for index, row in data.iterrows():
+#        centroid_assignments[assign_to_cluster(centroids, row, dist_measure)].append(index)
+#
+#    while True:
+#        # Recompute centroids
+#        for centroid in centroid_assignments:
+#            # Reset averages of all features to 0
+#            averages = []
+#            for i in range(len(data.columns)):
+#                averages.append(0)
+#            
+#            # Iterate over samples in this cluster,
+#            # adding their features to average
+#            for sample in centroid_assignments[centroid]:
+#                for i in range(len(data.columns)):
+#                    averages[i] += data.at[sample, data.columns[i]]
+#            
+#            # Divide all sums by number of samples in cluster to get average
+#            for i in range(len(averages)):
+#                if(len(centroid_assignments[centroid]) != 0):
+#                    averages[i] /= len(centroid_assignments[centroid])
+#            
+#            # Re-assign centroid values
+#            for i in range(len(centroids[centroid])):
+#                centroids[centroid][i] = averages[i]
+#        
+#        # Reassign data to clusters
+#        new_centroid_assignments = {}
+#        for i in range(K):
+#            new_centroid_assignments[i] = []
+#        
+#        for index, row in data.iterrows():
+#            new_centroid_assignments[assign_to_cluster(centroids, row, dist_measure)].append(index)
+#        
+#        # If clusters have not changed, we're done
+#        if new_centroid_assignments == centroid_assignments:
+#            return centroid_assignments
+#        else:
+#            centroid_assignments = new_centroid_assignments
         
     
-
+#TODO: Fix this to assume centroids is a dataframe
 def assign_to_cluster(centroids, sample, dist_measure):
     if dist_measure == "euclidean":
         # Find which centroid the sample is the closest to
